5861e9a889e5b5cc6bb3fdcb8b8c1496714c0d98
Crs <cristian.dias@terraviva.agr.br>
2022-05-30 19:59:02 -0300

    feat: Criação diretório 'telas'

---
+++ src/telas/Cesta.js
@@ -0,0 +1,8 @@
+import { Text } from 'react-native'
+
+/* export - Necessário utilizar 
+            para que seja possível utilizar em outros arquivos
+*/
+export default function Cesta() {
+    return <Text>Cesta</Text>
+}
