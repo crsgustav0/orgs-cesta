c6db3c251092986a3ed27e0afc7d97eafee0a770
Crs <cristian.dias@terraviva.agr.br>
2022-05-30 19:57:40 -0300

    feat: Importação classe 'Cesta', arquivo 'App.js'

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Cesta from './src/telas/Cesta'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Cesta />
       <StatusBar style="auto" />
     </View>
   );
