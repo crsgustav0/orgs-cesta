05564e231081c905b28110acedd79f959e19f1b2
Crs <cristian.dias@terraviva.agr.br>
2022-06-21 20:57:12 -0300

    feat: Importação de imagem cabeçalho, arquivo 'App.js'

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
 
 import Cesta from './src/telas/Cesta'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Cesta />
+    <SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+      <Cesta />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
