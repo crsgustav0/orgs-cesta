137faa08a4ac1363c75906a7d796a949c04e932a
Cristian Gustavo <cristian.dias@terraviva.agr.br>
2022-07-07 12:07:15 -0300

    feat: Criação diretório mocks

---
+++ src/mocks/cesta.js
@@ -0,0 +1,18 @@
+import logo from '../../assets/logo.png'
+
+const cesta = {
+    topo: {
+        titulo: 'Detalhes da cesta'
+    },
+    detalhes: {
+        nome: 'Cesta de verduras',
+        logoFazenda: logo,
+        nomeFazenda: 'Jenny Jack Farm',
+        descricao: 'Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha',
+        preco: 'R$40,00'
+    },
+
+}
+
+/*Exporta o objeto criado com as descrições dos componentes*/
+export default cesta;
